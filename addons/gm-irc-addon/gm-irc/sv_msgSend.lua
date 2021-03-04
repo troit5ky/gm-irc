add9ca914cff1f1eb29a59205f850cecc137c82d
testeron211 <testeron211@gmail.com>
2021-03-04 19:47:40 +0300

    player connection message in discord!

--- addons/gm-irc-addon/gm-irc/sv_msgSend.lua
+++ addons/gm-irc-addon/gm-irc/sv_msgSend.lua
@@ -42,6 +42,20 @@ function playerSpawnForTheFirstTime(ply, transit)
 	sendMsgToDiscord( json )
 end
 
+function playerConnect(ply, ip)
+	local embed = {
+		title = "Игрок " .. ply:GetName() .." (".. ply:SteamID() .. ") инициировал подключение",
+		color = 16763979
+	}
+
+	local json = util.TableToJSON ({
+			username = GM_IRC.Username,
+			embeds = {embed}
+		})
+	
+	sendMsgToDiscord( json )
+end
+
 function playerDisconnected(ply)
 	local embed = {
 		title = "Игрок " .. ply.name .." (".. ply.networkid .. ") отключился от сервера (" .. ply.reason .. ")",
@@ -72,8 +86,9 @@ function playersCount()
 	end
 end
 
-hook.Add("PlayerInitialSpawn", "playerspawnedtobot", playerSpawnForTheFirstTime)
-hook.Add( "PlayerSay", "gg", sendPost)
+hook.Add("PlayerConnect", "irc_onplayerconnect", playerConnect)
+hook.Add("PlayerInitialSpawn", "irc_onplayerinitspawn", playerSpawnForTheFirstTime)
+hook.Add( "PlayerSay", "irc_onplayersay", sendPost)
 gameevent.Listen( "player_disconnect" )
-hook.Add("player_disconnect", "ondisconnect", playerDisconnected)
+hook.Add("player_disconnect", "irc_ondisconnect", playerDisconnected)
 if GM_IRC.SendPlayersCount then timer.Create("irc_players_count", GM_IRC.PlayersCountDelay, 0, playersCount) end
